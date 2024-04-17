e8801936b7c243fe6ee60b671fdcb5f10a681e40
ksj <kofdx7@gmail.com>
2024-04-17 15:55:30 +0900

    edit rtb

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -22,9 +22,9 @@ module "pri_subnets" {
   for_each = merge(var.subnets,local.pri_subnets)
   subnet_config=each.value
 }
-#output "pub_subnet_ids" {
-#  value = flatten([for subnet_info in values(module.pub_subnets) : subnet_info.subnet_id])
-#}
+output "pub_subnet_ids" {
+  value = module.pub_subnets.subnet_id
+}
 #output "pri_subnet_ids" {
 #  value = flatten([for subnet_info in values(module.pri_subnets) : subnet_info.subnet_id])
 #}
