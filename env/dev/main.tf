c19917a13a3f78c9e99e421f30e2576c2dac414d
ksj <kofdx7@gmail.com>
2024-04-17 16:10:15 +0900

    edit rtb

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -32,7 +32,7 @@ output "pri_subnet_ids" {
 
 */
 output "pub_subnet_ids" {
-  value = module.pub_subnets.subnet_id
+  value = flatten(module.pub_subnets.subnet_id)
 }
 /*
 module "public_subnet1" {
--- module/subnet/outputs.tf
+++ module/subnet/outputs.tf
@@ -1,5 +1,3 @@
 output "subnet_id" {
-  #value = flatten([for subnet_info in values(aws_subnet.subnet) : subnet_info.id])
-
-  value = [for key, subnet in aws_subnet.subnet : subnet.id]
+  value = flatten([for subnet_info in values(aws_subnet.subnet) : subnet_info.id])
 }
