83d9f97a72bf7b20469132a5162f6d070a492b9f
ksj <kofdx7@gmail.com>
2024-04-17 17:00:13 +0900

    edit iam role

--- env/dev/vars.tf
+++ env/dev/vars.tf
@@ -71,7 +71,7 @@ variable "iam_roles"{
       name = "EKS_CLUSTER_ROLE"
       tag_name = "EKS_CLUSTER_ROLE"
       #assume_role_policy = data.aws_iam_policy_document.eks_cluster_role.json
-      assume_role_policy = data.aws_iam_policy_document.eks_cluster_role.json
+      assume_role_policy = "template/eks_cluster_role_policy.json"
       mgd_policies = [
         "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
         "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController",
