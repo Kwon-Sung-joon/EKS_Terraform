db1012288c20dda51867d6305d3773720251642b
ksj <kofdx7@gmail.com>
2024-04-17 16:01:36 +0900

    edit rtb

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -22,6 +22,7 @@ module "pri_subnets" {
   for_each = merge(var.subnets,local.pri_subnets)
   subnet_config=each.value
 }
+/*
 output "pub_subnet_ids" {
   value = flatten([for subnet_info in values(module.pub_subnets) : subnet_info.subnet_id])
 }
@@ -29,8 +30,9 @@ output "pri_subnet_ids" {
   value = flatten([for subnet_info in values(module.pri_subnets) : subnet_info.subnet_id])
 }
 
+*/
 output "test" {
-  value = module.pub_subnets.test
+  value = module.pub_subnets.subnet_id
 }
 /*
 module "public_subnet1" {
--- module/subnet/outputs.tf
+++ module/subnet/outputs.tf
@@ -1,5 +1,6 @@
 output "subnet_id" {
-  value = aws_subnet.subnet.id
+  #value = aws_subnet.subnet.id
+  value = flatten([for subnet_info in values(aws_subnet.subnet) : subnet_info.id])
 }
 
 output "test" {
