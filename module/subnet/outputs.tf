de71cde1a3daa5f20a5e179bdcadf7c9ebfc1a6d
ksj <kofdx7@gmail.com>
2024-04-17 16:09:10 +0900

    edit rtb

--- module/subnet/outputs.tf
+++ module/subnet/outputs.tf
@@ -1,4 +1,5 @@
 output "subnet_id" {
-  #value = aws_subnet.subnet.id
-  value = flatten([for subnet_info in values(aws_subnet.subnet) : subnet_info.id])
+  #value = flatten([for subnet_info in values(aws_subnet.subnet) : subnet_info.id])
+
+  value = [for key, subnet in aws_subnet.subnet : subnet.id]
 }
