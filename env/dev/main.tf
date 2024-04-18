1e33a9631d9593b1ff763dc107e272eea41e8d12
ksj <kofdx7@gmail.com>
2024-04-18 15:16:42 +0900

    edit eks cluster

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -91,7 +91,7 @@ output eks_node_group_role {
 }
 
 output eks_node_group_role2 {
-  value = values(module.eks_cluster_iam_role[0].iam_role)
+  value = values(module.eks_cluster_iam_role["dev_cluster"].iam_role)
 }
 /*
 module "eks_node_lt" {
