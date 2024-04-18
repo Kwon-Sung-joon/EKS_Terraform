c872b636ef1cbdd8b86cef40807c3038bce22cde
ksj <kofdx7@gmail.com>
2024-04-18 14:21:10 +0900

    edit eks cluster

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -67,7 +67,7 @@ module "eks_cluster" {
   depends_on = [
     module.eks_cluster_iam_role
   ]
-  eks-cluster-role    = module.eks_cluster_iam_role.iam_role["dev_node_group"]
+  eks-cluster-role    = module.eks_cluster_iam_role["dev_node_group"].iam_role
   eks-cluster-version = 1.26
   vpc_cidr = var.vpc_cidr
 }
