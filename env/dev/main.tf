fe6dfdd97b3b0659a1473d3f5d01a6800d2928bb
ksj <kofdx7@gmail.com>
2024-04-18 17:30:54 +0900

    edit eks cluster

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -61,7 +61,11 @@ module "eks_node_groups" {
 /*
 module "eks_cluster" {
   source            = "../../module/eks_cluster"
-  subnet_ids        = flatten([for subnet_info in values(module.pub_subnets) : subnet_info.subnet_id])
+  subnet_ids        = flattenconcat(module.pub_subnets["pub1"].subnet_id,
+    module.pub_subnets["pub2"].subnet_id
+    module.pri_subnets["pri1"].subnet_id
+    module.pri_subnets["pri2"].subnet_id
+  )
   alltag            = var.alltag
   service_ipv4_cidr = var.eks_cluster_service_ipv4_cidr
   depends_on = [
@@ -92,7 +96,11 @@ output eks_node_group_role {
 
 output eks_node_group_role2 {
   #value = concat(flatten([for subnet in module.pub_subnets : subnet.subnet_id]),flatten([for subnet in module.pri_subnets : subnet.subnet_id]))
-  value = concat(module.pub_subnets["pub1"].subnet_id,module.pub_subnets["pub2"].subnet_id)
+  value = concat(module.pub_subnets["pub1"].subnet_id,
+    module.pub_subnets["pub2"].subnet_id
+    module.pri_subnets["pri1"].subnet_id
+    module.pri_subnets["pri2"].subnet_id
+  )
 }
 /*
 module "eks_node_lt" {
