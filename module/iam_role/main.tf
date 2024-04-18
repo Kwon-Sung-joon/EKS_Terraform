8b762471942f568bca1b5f321790bc3e18e1d137
ksj <kofdx7@gmail.com>
2024-04-18 09:29:41 +0900

    edit iam role

--- module/iam_role/main.tf
+++ module/iam_role/main.tf
@@ -9,8 +9,12 @@ resource "aws_iam_role" "iam_role" {
 
 resource "aws_iam_role_policy_attachment" "aws_iam_policy_attach" {
   role       = aws_iam_role.iam_role.name
-  for_each   = tolist(var.iam_role_config.mgd_policies)
+  for_each   = var.iam_role_config.mgd_policies
   policy_arn = each.value
+}
+
+output "test" {
+  value=var.iam_role_config.mdg_policies
 }
 output "iam_role" {
   value = aws_iam_role.iam_role.arn
