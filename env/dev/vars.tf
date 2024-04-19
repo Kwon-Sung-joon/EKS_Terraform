41f022ee4188ed8d8d8b747395caee277490acae
ksj <kofdx7@gmail.com>
2024-04-19 10:24:32 +0900

    edit security groups

--- env/dev/vars.tf
+++ env/dev/vars.tf
@@ -161,7 +161,7 @@ locals {
           cidr_ipv4   = "0.0.0.0/0"
           from_port   = 0
           ip_protocol = "tcp"
-          to_port     = 0
+          to_port     = 65535
           description = "outbound_80"
         }
       }
