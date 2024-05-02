df322a245478070cf1acf154d1dc1b3ae07e8918
ksj <kofdx7@gmail.com>
2024-05-02 14:18:07 +0900

    add admin instance

--- env/dev/user_data/ec2_eks_admin.sh
+++ env/dev/user_data/ec2_eks_admin.sh
@@ -24,14 +24,24 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash
+
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 
+dnf install -y docker
+systemctl start docker
+systemctl enable docker
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.12/2024-01-04/bin/linux/arm64/kubectl
-chmod +x /usr/local/bin/kubectl
-echo "alias k='kubectl'" >> ~/.bash_profile
-source ~/.bash_profile
+usermod -a -G docker ec2-user
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.12/2024-01-04/bin/linux/arm64/kubectl	# 버전 1.24
+
+chmod +x ./kubectl
+
+HOME=/home/ec2-user
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
 dnf install -y jq
 dnf install -y bash-completion
 aws eks update-kubeconfig --region ap-northeast-2 --name ${CLUSTER-NAME}
