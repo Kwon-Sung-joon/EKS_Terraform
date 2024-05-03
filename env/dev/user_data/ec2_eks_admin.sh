cd7dbbff816b29e8cd04e92e2356de8c8e1e3186
ksj <kofdx7@gmail.com>
2024-05-03 17:43:25 +0900

    kubectl update

--- env/dev/user_data/ec2_eks_admin.sh
+++ env/dev/user_data/ec2_eks_admin.sh
@@ -44,4 +44,4 @@ kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 
 dnf install -y jq
 dnf install -y bash-completion
-aws eks update-kubeconfig --region ap-northeast-2 --name ${CLUSTER-NAME}
+su ec2-user "aws eks update-kubeconfig --region ap-northeast-2 --name ${CLUSTER-NAME}"
