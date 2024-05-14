f6cd7961c5c1791b6b61e50c32ebce0723c56aa9
ksj <kofdx7@gmail.com>
2024-05-14 17:16:22 +0900

    test

--- env/dev/data.tf
+++ env/dev/data.tf
@@ -35,6 +35,11 @@ data "aws_availability_zones" "available" {
 data "tls_certificate" "dev_eks_cluster_1_oidc" {
   url = module.eks_cluster["dev_cluster_1"].cluster_oidc
 }
+
+
+
+
+
 /*
 data "template_file" "eks_userdata" {
   template = "./user_data/eks_node.sh"
