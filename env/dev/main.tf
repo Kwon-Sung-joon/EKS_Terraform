e4224e5b89a074bdc26c7b1903f338b3507acee9
ksj <kofdx7@gmail.com>
2024-05-14 13:35:28 +0900

    add iam oidc

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -80,7 +80,7 @@ module "iam_oidc" {
 }
 */
 output eks_oidc {
-  value = module.eks_cluster["dev_cluster_1"].cluster_oidc["oidc"]
+  value = module.eks_cluster["dev_cluster_1"].cluster_oidc
 }
 /*
 module "eks_node_group" {
