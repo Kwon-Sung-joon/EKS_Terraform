c7aa7c8ae2c83bd337d2e918f60840b33525a4d4
ksj <kofdx7@gmail.com>
2024-05-16 15:42:22 +0900

    test ingress alb

--- env/dev/data.tf
+++ env/dev/data.tf
@@ -46,20 +46,20 @@ data "aws_iam_policy_document" "dev_elb_sa_role" {
   statement {
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/arn:aws:iam::<ACCOUNT>>:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC"]
+      identifiers = ["arn:aws:iam::${var.account_id}:oidc-provider/arn:aws:iam::<ACCOUNT>:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC"]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     condition {
       test     = "StringEquals"
-      variable = "arn:aws:iam::672956273056:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC:aud"
+      variable = "arn:aws:iam::<ACCOUNT>:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC:aud"
       values   = ["sts.amazonaws.com"]
     }
 
     condition {
       test     = "StringEquals"
-      variable = "arn:aws:iam::672956273056:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC:sub"
+      variable = "arn:aws:iam::<ACCOUNT>:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/42AA06C0891D619DB25EEF90FD53D2AC:sub"
       values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
     }
   }
