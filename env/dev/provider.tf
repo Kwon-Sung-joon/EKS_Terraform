423bd3d933b522a9ba99eaa8f154a19d1d7ffeca
ksj <kofdx7@gmail.com>
2024-05-16 11:22:26 +0900

    add helm

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -13,11 +13,30 @@ terraform {
   }
 }
 
-provider "helm" {
-    config_raw = base64decode(data.aws_s3_bucket_object.kubeconfig.body)
+provider "kubernetes" {
+  host                   = module.eks_cluster["dev_cluster_1"].endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster["dev_cluster_1"].kubeconfig-certificate-authority-data)
+  config_path    = ""
+  config_context = ""
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", "dev_cluster_1"]
+  }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster["dev_cluster_1"].endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster["dev_cluster_1"].kubeconfig-certificate-authority-data)
+    config_path    = ""
+    config_context = ""
 
-provider "kubernetes" {
-  config_raw = base64decode(data.aws_s3_bucket_object.kubeconfig.body)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", "dev_cluster_1"]
+    }
+  }
 }
