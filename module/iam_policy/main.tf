7b3053003366e7347a38232757c0bc628b15604a
ksj <kofdx7@gmail.com>
2024-05-22 13:10:36 +0900

    add karpenter env

--- module/iam_policy/main.tf
+++ module/iam_policy/main.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "policy" {
   name        = var.iam_policy_config.name
   description = var.iam_policy_config.description
-  policy = file(var.iam_policy_config.policy)
+  policy = var.iam_policy_config.policy
   tags = var.iam_policy_config.tags
 }
 
