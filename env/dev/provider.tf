d362989fb6ca6d7e46a36d15e2a646773a15ccae
ksj <kofdx7@gmail.com>
2024-05-28 09:29:37 +0900

    test karpenter

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -12,9 +12,6 @@ terraform {
     encrypt = true
   }
 }
-
-
-
 provider "kubernetes" {
   host                   = module.eks_cluster["dev_cluster_1"].endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster["dev_cluster_1"].kubeconfig-certificate-authority-data)
@@ -27,15 +24,12 @@ provider "kubernetes" {
     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster["dev_cluster_1"].cluster_name]
   }
 }
-
-
 provider "helm" {
   kubernetes {
     host                   = module.eks_cluster["dev_cluster_1"].endpoint
     cluster_ca_certificate = base64decode(module.eks_cluster["dev_cluster_1"].kubeconfig-certificate-authority-data)
     config_path    = ""
     config_context = ""
-
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
--- env/dev/vars.tf
+++ env/dev/vars.tf
@@ -305,10 +305,10 @@ locals {
         Owner = "ksj"
       }
       route =[
-        {
-          cidr_block = "0.0.0.0/0"
-          gateway_id = module.nat_gw["dev_nat_gw_1"].nat_gw
-        }
+#        {
+#          cidr_block = "0.0.0.0/0"
+#          gateway_id = module.nat_gw["dev_nat_gw_1"].nat_gw
+#        }
       ]
       subnets = [module.subnets["pri1"].subnet_id,module.subnets["pri2"].subnet_id]
     }
@@ -529,7 +529,7 @@ locals {
     dev_eks_node_groups_lt = {
       name = "dev-eks-ng-lt"
       image_id = "ami-06aaf7c21e7e74e2a"
-      instance_type = "t3.medium"
+      instance_type = "t4g.medium"
       update_default_version = false
       vpc_security_group_ids = [module.security_groups["dev_eks_node_sg"].id]
       user_data = base64encode(templatefile("${path.module}/user_data/eks_node.sh",
