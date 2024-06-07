f515cab5dd28a5bb81afd509136d2e0a508c340c
ksj <kofdx7@gmail.com>
2024-06-07 16:49:31 +0900

    upgrade terraform version for manifest_decode_multi

--- env/main/main.tf
+++ env/main/main.tf
@@ -136,15 +136,15 @@ module "helm_release" {
   helm_release_config = each.value
   depends_on = [module.k8s_karpenter]
 }
-/*
+
 ##K8S Resources
-module "k8s_service_account" {
+module "k8s_manifest" {
   source = "../../module/k8s_manifest"
   for_each = merge(var.k8s_manifest,local.DEV_K8S_MANIFEST)
   k8s_manifest_config = each.value
   depends_on = [module.helm_release]
 }
-*/
+
 
 
 /*
