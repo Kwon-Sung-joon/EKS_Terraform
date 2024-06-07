99d9ade65a6b4c2842ddd09cac26fba7deb058db
ksj <kofdx7@gmail.com>
2024-06-07 11:20:30 +0900

    add eks cluster addons

--- module/eks_cluster_add_on/main.tf
+++ module/eks_cluster_add_on/main.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "addon" {
-  cluster_name                = var.eks_cluster_addon_config.name
+  cluster_name                = var.eks_cluster_addon_config.cluster_name
   addon_name                  = var.eks_cluster_addon_config.addon_name
   addon_version               = var.eks_cluster_addon_config.addon_version
   resolve_conflicts_on_create = var.eks_cluster_addon_config.resolve_conflicts_on_create
