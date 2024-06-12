139d38f753ffddb6af1a636c33115e663be37915
ksj <kofdx7@gmail.com>
2024-06-12 11:01:02 +0900

    upgrade terraform version for manifest_decode_multi

--- module/k8s_manifest/main.tf
+++ module/k8s_manifest/main.tf
@@ -2,3 +2,4 @@ resource "kubernetes_manifest" "k8s_manifest" {
   for_each = { for m in provider::kubernetes::manifest_decode_multi(var.k8s_manifest_config.manifest):m.metadata.name => m }
   manifest = each.value
 }
+
